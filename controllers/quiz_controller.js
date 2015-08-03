f0c4ce390cf442a8f8f7afd5e741469864c992e8
Miguel Cruzado <macmanzanero@gmail.com>
2015-08-03 11:23:46 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -38,3 +38,19 @@ exports.index = function(req, res) {
 		res.render('quizes/index.ejs', { quizes: quizes});
 	}).catch(function(error) {next(error);})
 };
+
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build(
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	})
+};
